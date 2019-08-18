8b41a7936716fa056311f67738b5a46ae2c7b2ed
AlexV531 <v.alexander.531@gmail.com>
2019-08-18 15:51:32 -0400

    Added Movement

--- public/js/main.js
+++ public/js/main.js
@@ -13,8 +13,11 @@ const SCALE = 10.0
 const BG_COLOR = '#FFFF00'
 /** Radius of circle */
 const CIRCLE_RADIUS = 1.0
-
+const SPEED = 2 
 const position = {
+	x:0, y:0
+}
+const velocity = {
 	x:0, y:0
 }
 let prevT = Date.now()
@@ -29,10 +32,42 @@ function resize() {
 	render()
 }
 
+/** @param {KeyboardEvent} e */
+function handleKeyDown(e) {
+	const code = e.keyCode 
+	if(code === 38){
+		velocity.y = SPEED
+	} else if (code === 40){
+		velocity.y = -SPEED
+	} else if (code === 37){
+		velocity.x = -SPEED
+	} else if (code === 39){
+		velocity.x = SPEED
+	}
+
+} 
+
+/** @param {KeyboardEvent} e */
+function handleKeyUp(e) {
+	const code = e.keyCode 
+	if(code === 38){
+		velocity.y = 0
+	} else if (code === 40){
+		velocity.y = 0
+	} else if (code === 37){
+		velocity.x = 0
+	} else if (code === 39){
+		velocity.x = 0
+	}
+
+} 
+
 /** Call this once on application startup */
 function initApp() {
 	// Listen for window resize events
 	window.addEventListener('resize', resize)
+	window.addEventListener('keydown', handleKeyDown)
+	window.addEventListener('keyup', handleKeyUp)
 	resize()
 }
 
@@ -66,8 +101,11 @@ function update() {
 	const deltaT = curT - prevT
 	const fT = deltaT/1000
 	//position.x = position.x + fT * 1
-	position.x = Math.tan(curT/1000)
-	position.y = Math.sin(curT/1000) + Math.sin(curT/100)
+	//position.x = Math.tan(curT/1000)
+	//position.y = Math.sin(curT/1000) + Math.sin(curT/100)
+	position.x += velocity.x * fT
+	position.y += velocity.y * fT
+
 	prevT = curT
 	render()
 	requestAnimationFrame(update)
